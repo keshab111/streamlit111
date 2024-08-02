0e587bbf124688ce260f60879122bbd4af7f7f7b
keshab111 <107114250+keshab111@users.noreply.github.com>
2024-08-02 22:00:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 # Title
 st.title("Solar Power Generation Prediction")
