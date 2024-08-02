b10ebec4edb20d69d91086c2ec028739a2071e67
keshab111 <107114250+keshab111@users.noreply.github.com>
2024-08-02 21:48:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ st.header("Questions")
 st.write("Open Floor for Questions")
 
         # Logistic Regression
-        st.header('Logistic Regression')
+        st.header("Logistic Regression")
         regressor = LogisticRegressor(df)
         regressor.logistic()
         st.markdown('<hr/>', unsafe_allow_html=True)
